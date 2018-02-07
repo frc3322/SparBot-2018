494f7dc3247b944e3253d7c6805e2a2c59610766
tcw321@gmail.com <tcw321@gmail.com>
2018-02-06 20:49:12 -0500

    added grab command

---
+++ src/org/usfirst/frc/team3322/robot/commands/Grab.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team3322.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Grab extends Command {
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
