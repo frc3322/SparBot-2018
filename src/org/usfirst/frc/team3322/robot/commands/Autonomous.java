1803468a785fef128ae294bd4d0102dae08091d2
Jonathan Park <2020parkjonathan@aaps.k12.mi.us>
2018-06-14 19:22:40 -0400

    Added enum for starting positions

--- src/org/usfirst/frc/team3322/robot/commands/Autonomous.java
+++ src/org/usfirst/frc/team3322/robot/commands/Autonomous.java
@@ -3,10 +3,29 @@ package org.usfirst.frc.team3322.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class Autonomous extends CommandGroup{
+
+	public enum Position {
+		LEFT,
+		CENTER,
+		RIGHT
+	}
+
+	private Position startPos;
+
 	public Autonomous() {
+
+		this.startPos = Position.LEFT;
 		//addSequential(new DriveDistance(-20));
 		//addSequential(new RotateRobot(90));
-		addSequential(new DriveDistance(-8));
+		//addSequential(new DriveDistance(-8));
+		switch(Position){
+			case LEFT:
+			case RIGHT:
+				addSequential(new DriveDistance(-8));
+				break;
+			case CENTER:
+				//null
+		}
 	}
 
 }
